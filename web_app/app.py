116d9b7201e9881c40d7b0939ae470141d258b2c
Conor Hanson <condorhanson@gmail.com>
2022-08-09 07:56:44 -0500

    added stuff

--- web_app/app.py
+++ web_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template
 import pandas as pd
 import numpy as np
 import os
--- web_app/assets/js/logic.js
+++ web_app/assets/js/logic.js
@@ -1,1 +1,16 @@
-var somestufftomakeitwork
+$(document).ready(function() {
+    console.log("Ready to predict...");
+
+    $("#predict").click(function() {
+        predict_ml();
+    })
+});
+
+function predict_ml() {
+    var env_sat = $("env_sat").val();
+    var role = $("#role").val();
+    var dept = $("#dept").val();
+    var travel = $("#travel").val();
+
+
+}

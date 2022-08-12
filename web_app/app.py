f22bc3f5c110c0b9051ab8589a8c4c3321a73cde
Conor Hanson <condorhanson@gmail.com>
2022-08-11 19:13:00 -0500

    added page

--- web_app/app.py
+++ web_app/app.py
@@ -15,25 +15,31 @@ def home():
 @app.route("/predictionengine")
 def predict():
     
-    return render_template('/predictionengine')
+    return render_template('/prediction.html')
 
 ## app route to data viz
 @app.route("/tableauviz")
 def viz():
     
-    return render_template('/tableauviz')
+    return render_template('/dataviz.html')
 
 ## define app route to about us page
 @app.route("/aboutus")
 def about():
     
-    return render_template('/aboutus')
+    return render_template('/aboutus.html')
 
 ## app route to works cited page
 @app.route("/workscited")
 def works():
     
-    return render_template('/workscited')
+    return render_template('/workscited.html')
+
+## app route to data report page
+@app.route("/datareport")
+def works():
+    
+    return render_template('/documentation.html')
 
 if __name__ == "__main_":
     app.run(debug=True)

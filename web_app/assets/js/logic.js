ea3ea6152c326a6042de3a7ef1e2e50ceabf857a
Conor Hanson <condorhanson@gmail.com>
2022-08-12 16:32:54 -0500

    building it!

--- web_app/assets/js/logic.js
+++ web_app/assets/js/logic.js
@@ -6,17 +6,64 @@ $(document).ready(function() {
     });
 });
 
+// call Flask API endpoint
 function makePredictions() {
-    var env_sat = $("env_sat").val();
     var role = $("#role").val();
-    var dept = $("#dept").val();
     var travel = $("#travel").val();
-    
+    var dept = $("#dept").val();
+    var dist = $("#dist").val();
+    var ed = $("#ed").val();
+    var gender = $("#gender").val();
+    var joblevel = $("#joblevel").val();
+    var marr = $("#marr").val();
+    var income = $("#income").val();
+    var prevcomp = $("#companies").val();
+    var hoursworked = $("#stdhrs").val();
+    var stonks = $("#stonks").val();
+    var totalyears = $("#yearsworked").val();
+    var atcompany = $("#atcompany").val();
+    var lastpromotion = $("#promotion").val();
+    var currmanager = $("#manager").val();
+    var enviro = $("#enviro").val();
+    var jobsatisfaction = $("#satisfaction").val();
+    var balance = $("#balance").val();
+    var involvement = $("#involvement").val();
+    var performance = $("#performance").val();
+    var training = $("#training").val();
+    var edfield = $("#edfield").val();
+    var age = $("#age").val();
+    var salincrease = $("#salincrease").val();
 
+    // compile the data to send off
     var payload = {
-        "Age": age
+        "Age": age,
+        "BusinessTravel": travel,
+        "Department": dept,
+        "DistanceFromHome": dist,
+        "Education": ed,
+        "EducationField": edfield,
+        "Gender": gender,
+        "JobLevel": joblevel,
+        "JobRole": role,
+        "MaritalStatus": marr,
+        "MonthlyIncome": income,
+        "NumCompaniesWorked": prevcomp,
+        "PercentSalaryHike": salincrease,
+        "StandardHours": hoursworked,
+        "StockOptionLevel": stonks,
+        "TotalWorkingYears": totalyears,
+        "TrainingTimesLastYear": training,
+        "YearsAtCompany": atcompany,
+        "YearsSinceLastPromotion": lastpromotion,
+        "YearsWithCurrManager": currmanager,
+        "EnvironmentSatisfaction": enviro,
+        "JobSatisfaction": jobsatisfaction,
+        "WorkLifeBalance": balance,
+        "JobInvolvement": involvement,
+        "PerformanceRating": performance
     }
 
+    // Perform a POST request to the query URL
     $.ajax({
         type: "POST",
         url: "/makePredictions",
@@ -26,8 +73,11 @@ function makePredictions() {
             // print it
             console.log(returnedData);
 
-            
-
+            if (returnedData["prediction"] == 1) {
+                $("#output").text("You Survived!");
+            } else {
+                $("#output").text("You Died!");
+            }
         },
         error: function(XMLHttpRequest, textStatus, errorThrown) {
             alert("Status: " + textStatus);

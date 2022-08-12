7a774a96cec6a62aca55bc582e7737f47bfcc4c1
Conor Hanson <condorhanson@gmail.com>
2022-08-11 19:47:32 -0500

    writing web app stuff

--- web_app/app.py
+++ web_app/app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, jsonify
 import pandas as pd
 import numpy as np
 import os
 
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 ## define app route to root page
 @app.route("/")
@@ -12,34 +13,55 @@ def home():
     return render_template('index.html')
 
 ## app route to prediction engine
-@app.route("/predictionengine")
-def predict():
+@app.route("/prediction")
+def prediction():
     
     return render_template('/prediction.html')
 
 ## app route to data viz
-@app.route("/tableauviz")
-def viz():
+@app.route("/dataviz")
+def dataviz():
     
     return render_template('/dataviz.html')
 
 ## define app route to about us page
 @app.route("/aboutus")
-def about():
+def aboutus():
     
     return render_template('/aboutus.html')
 
 ## app route to works cited page
 @app.route("/workscited")
-def works():
+def workscited():
     
     return render_template('/workscited.html')
 
 ## app route to data report page
 @app.route("/datareport")
-def works():
+def documentation():
     
     return render_template('/documentation.html')
+
+## Prediction post receiver
+@app.route("/makePredictions")
+def makePredictions():
+    content = request.json['data']
+    print(content)
+    
+    return(jsonify({"ok": True}))
+
+####################################################################
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate, public, max-age=0"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    return r
 
 if __name__ == "__main_":
     app.run(debug=True)
--- web_app/assets/js/logic.js
+++ web_app/assets/js/logic.js
@@ -2,15 +2,35 @@ $(document).ready(function() {
     console.log("Ready to predict...");
 
     $("#predict").click(function() {
-        predict_ml();
-    })
+        makePredictions();
+    });
 });
 
-function predict_ml() {
+function makePredictions() {
     var env_sat = $("env_sat").val();
     var role = $("#role").val();
     var dept = $("#dept").val();
     var travel = $("#travel").val();
     
+
+    var payload = {
+        "Age": age
+    }
+
+    $.ajax({
+        type: "POST",
+        url: "/makePredictions",
+        contentType: 'application/json;charset=UTF-8',
+        data: JSON.stringify({ "data": payload }),
+        success: function(returnedData) {
+            // print it
+            console.log(returnedData);
+
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+            alert("Status: " + textStatus);
+            alert("Error: " + errorThrown);
+        }
+    });
 
 }

b3266e736049780903fae853d437e7a40cd17aa6
Conor Hanson <condorhanson@gmail.com>
2022-08-16 20:20:31 -0500

    working...

--- web_app/modelHelper.py
+++ web_app/modelHelper.py
@@ -8,14 +8,14 @@ class ModelHelper():
     def __init__(self):
         pass
 
-    def makePredictions(age, travel, department, dist, 
+    def makePredictions(self, age, travel, department, dist, 
                         ed, edfield, gender, joblvl, role, marriage,
                         monthincome, numcoworked, pctsalhike, stdhours,
                         stonkopt, totalwrkyrs, training, yrsatco, lastpromo,
                         yearsmanag, envsat, jobsat, worklife, jobinv, perf):
                         
         # Parse Parse Parse!!
-
+        
 
         input_pred = [[age, travel, department, dist, 
                         ed, edfield, gender, joblvl, role, marriage,
@@ -23,12 +23,11 @@ class ModelHelper():
                         stonkopt, totalwrkyrs, training, yrsatco, lastpromo,
                         yearsmanag, envsat, jobsat, worklife, jobinv, perf]]
 
-
         filename = 'finalized_model.sav'
-        ada_load = pickle.load(open(filename, 'rb'))
+        model = pickle.load(open(filename, 'rb'))
 
         X = np.array(input_pred)
-        preds = ada_load.predict_proba(X)
-        preds_singular = ada_load.predict(X)
+        preds = model.predict_proba(X)
+        preds_singular = model.predict(X)
 
         return preds_singular[0]
